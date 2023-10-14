fbf2783ebb2b7495a77be42ddb996807cfa72afe
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-14 20:34:48 +0600

    custom dialog add

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,68 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
           title: Text("Custom Dialog Demo"),
           backgroundColor: Colors.pinkAccent),
+      body: Container(
+        alignment: Alignment.center,
+        child: OutlinedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) {
+                  return Dialog(
+                    backgroundColor: Colors.white,
+                    surfaceTintColor: Colors.transparent,
+                    child: Stack(
+                        alignment: Alignment.topCenter,
+                        clipBehavior: Clip.none,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(16, 40, 16, 16),
+                            height: 250,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 20),
+                                Text("Custom Dialog",
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontFamily: "Segoe UI",
+                                        color: Colors.black)),
+                                SizedBox(height: 12),
+                                Text(
+                                    "In Android, A dialog is a small window that prompts the user to make a decision, provide some additional information, and inform the user about some particular task",
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontFamily: "Segoe UI",
+                                        color: Colors.black)),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(backgroundColor: Colors.cyan),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("close", style: TextStyle( color: Colors.white),)),
+                                )
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                              top: -50,
+                              child: CircleAvatar(
+                                radius: 50,
+                                child: Image.asset("assets/profile.png"),
+                                backgroundColor: Colors.cyan,
+                              ))
+                        ]),
+                  );
+                });
+          },
+          child: Text("Show Dialog"),
+        ),
+      ),
     );
   }
 }
